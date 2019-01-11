c2b32b19095c96a679682c1c16c406131730bf0a
devopsvc <45538349+devopsvc@users.noreply.github.com>
2019-01-11 20:17:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,5 @@
 echo "This is a simple php application";
 echo "<br>another line";
 echo "<br>another line";
-echo "<br>SKR PHP for openshift";
-echo "<br>nikil PHP for openshift";
-echo "<br>maha PHP for openshift";
+
 
